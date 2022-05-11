feed751db1feecc0c316004daf369007c1940c5d
Your Name <kewa990809@gmail.com>
2022-05-10 22:17:40 -0400

    kaggle

---
+++ test.py
@@ -0,0 +1,44 @@
+for i in range(0,28):
+    print(str(i)+'.jpg,0')
+for i in range(28,55):
+    print(str(i)+'.jpg,1')
+for i in range(55,84):
+    print(str(i)+'.jpg,2')
+for i in range(84,101):
+    print(str(i)+'.jpg,3')
+for i in range(101,130):
+    print(str(i)+'.jpg,4')
+for i in range(130,147):
+    print(str(i)+'.jpg,5')
+for i in range(147,172):
+    print(str(i)+'.jpg,6')
+for i in range(172,196):
+    print(str(i)+'.jpg,7')
+for i in range(196,223):
+    print(str(i)+'.jpg,8')
+for i in range(223,242):
+    print(str(i)+'.jpg,9')
+for i in range(242,263):
+    print(str(i)+'.jpg,10')
+for i in range(263,290):
+    print(str(i)+'.jpg,11')
+for i in range(290,307):
+    print(str(i)+'.jpg,12')
+for i in range(307,327):
+    print(str(i)+'.jpg,13')
+for i in range(327,352):
+    print(str(i)+'.jpg,14')
+for i in range(352,377):
+    print(str(i)+'.jpg,15')
+for i in range(377,398):
+    print(str(i)+'.jpg,16')
+for i in range(398,427):
+    print(str(i)+'.jpg,17')
+for i in range(427,452):
+    print(str(i)+'.jpg,18')
+for i in range(452,469):
+    print(str(i)+'.jpg,19')
+for i in range(469,492):
+    print(str(i)+'.jpg,20')
+for i in range(492,517):
+    print(str(i)+'.jpg,21')
